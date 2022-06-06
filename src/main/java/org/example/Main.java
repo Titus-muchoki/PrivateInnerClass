3ecb48308048aa1f4da7315bb9208ef3ab979ee6
kajela <titoyut56@gmail.com>
2022-06-06 15:45:11 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+
+     class outterClass{
+         int age = 23;
+          class innerClass{
+             String name = "kajela";
+         }
+        }
+    class Main {
+         public static void main(String[] args) {
+             outterClass myObj = new outterClass();
+             outterClass.innerClass myObj1 = myObj.new innerClass();
+        System.out.println(myObj1.name + myObj.age);
     }
 }
